d4c172990b518cdba1170ae834c6874c3da45617
Henner Zeller <h.zeller@acm.org>
2025-08-22 15:31:30 +0200

    Indicate print direction; allow ascii-escape print.

--- bidi-tee-print.cc
+++ bidi-tee-print.cc
@@ -1,24 +1,30 @@
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <time.h>
-#include <unistd.h>
+#include <cstdlib>
+#include <unistd.h>  // NOLINT for getopt
 
 #include <cerrno>
+#include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <ctime>
 
 #include <algorithm>
 #include <set>
 
-
 #include "block-header.h"
+
+struct PrintOptions {
+  bool colored = true;
+  bool ascii_escape = false;
+  bool ascii_escape_break_after_newline = true;
+};
 
 static int usage(const char *progname, int retval) {
   fprintf(stderr, "Usage: %s [<options>] <bidi-tee-logfile>\n", progname);
   fprintf(stderr,
           "-h            : this help\n"
           "-c            : toggle print in color (default: on)\n"
+          "-e            : toggle c-escape output (default: off)\n"
+          "-n            : if -e: do start new line after '\\n' (default: on)\n"
           "-ts           : Print timestamp since start of recording.\n"
           "-ta           : Print timestamps as absolute timestamps.\n"
           "-td           : Print delta timestamps relative to last print\n"
@@ -35,17 +41,55 @@ static constexpr const char *kColors[3] = {
     "",           // regular
 };
 
-static constexpr char kSuffix[] = "\033[0m";
+static constexpr char kResetColor[] = "\033[0m";
 
-void write_colored(FILE *out, int channel, const char *buf, size_t size) {
-  if (channel < 3) {
+static void PrintCEscaped(FILE *out, const PrintOptions &opts,
+                          const char *content, size_t size) {
+  const char *const end = content + size;
+  for (/**/; content < end; ++content) {
+    const char c = *content;
+    switch (c) {
+    case '\n':
+      if (opts.ascii_escape_break_after_newline) {
+        fwrite("\\n\n", 3, 1, out);
+      } else {
+        fwrite("\\n", 2, 1, out);
+      }
+      break;
+    case '\r': fwrite("\\r", 2, 1, out); break;
+    case '\t': fwrite("\\t", 2, 1, out); break;
+    default:
+      if (c < ' ') {
+        fprintf(out, "\\x%02x", c);
+      } else {
+        fwrite(&c, 1, 1, out);  // todo: collect in bulk if no escapes.
+      }
+    }
+  }
+}
+
+static void PrintContent(FILE *out, const PrintOptions &opts, int channel,
+                         const char *content, size_t size) {
+  if (opts.colored && channel < 3) {
     fwrite(kColors[channel], 1, strlen(kColors[channel]), out);
   }
   // TODO: apply color line by line as less is not be able to do multiline
-  fwrite(buf, 1, size, out);
-  fwrite(kSuffix, 1, strlen(kSuffix), out);
+  if (opts.ascii_escape) {
+    PrintCEscaped(out, opts, content, size);
+  } else {
+    fwrite(content, size, 1, out);
+  }
+  if (opts.colored) fwrite(kResetColor, strlen(kResetColor), 1, out);
 }
 
+static const char *PrintChannel(int channel) {
+  switch (channel) {
+  case 0: return "->";  // stdin
+  case 1: return "<-";  // stdout
+  case 2: return "<=";  // stderr
+  default: return "??";
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -56,29 +100,54 @@ int main(int argc, char *argv[]) {
   std::set<int> selected_channels;
   enum class TSPrint { kNone, kStartFile, kDelta, kAbsolute };
   TSPrint print_timestamp = TSPrint::kNone;
-  bool print_colored = true;
+  PrintOptions print_opts;
 
   int opt;
-  while ((opt = getopt(argc, argv, "ht:co:s:")) != -1) {
+  while ((opt = getopt(argc, argv, "ht:ceno:s:")) != -1) {
     switch (opt) {
-    case 'h': return usage(argv[0], 0);
-    case 't': switch (optarg[0]) {
-      case 's': print_timestamp = TSPrint::kStartFile; break;
-      case 'a': print_timestamp = TSPrint::kAbsolute; break;
-      case 'd': print_timestamp = TSPrint::kDelta; break;
+    case 'h':
+      return usage(argv[0], 0);
+    case 't':
+      switch (optarg[0]) {
+      case 's':
+        print_timestamp = TSPrint::kStartFile;
+        break;
+      case 'a':
+        print_timestamp = TSPrint::kAbsolute;
+        break;
+      case 'd':
+        print_timestamp = TSPrint::kDelta;
+        break;
       default:
         fprintf(stderr, "-t requires a letter to qualify timestamp printing\n");
         return usage(argv[0], 2);
       }
       break;
-    case 'c': print_colored = !print_colored; break;
-    case 'o': out = fopen(optarg, "wb"); break;
+    case 'c':
+      print_opts.colored = !print_opts.colored;
+      break;
+    case 'e':
+      print_opts.ascii_escape = !print_opts.ascii_escape;
+      break;
+    case 'n':
+      print_opts.ascii_escape_break_after_newline =
+          !print_opts.ascii_escape_break_after_newline;
+      break;
+    case 'o':
+      out = fopen(optarg, "wb");
+      break;
     case 's': {
-      int s[4];   // In case there will be an extra channel.
-      int count = sscanf(optarg, "%d,%d,%d,%d", &s[0], &s[1], &s[2], &s[3]);
-      for (int i = 0; i < count; ++i) selected_channels.insert(s[i]);
+      int s[4]; // In case there will be an extra channel.
+      const int num = sscanf(optarg, "%d,%d,%d,%d", &s[0], &s[1], &s[2], &s[3]);
+      for (int i = 0; i < num; ++i) {
+        selected_channels.insert(s[i]);
     }
+    } break;
+
       // ΤODO: case 'f' for 'follow'
+
+    default:
+      return usage(argv[0], 2);
     }
   }
 
@@ -104,35 +173,38 @@ int main(int argc, char *argv[]) {
   bool last_was_newline = true;
   char delta_timestamp_prefix = ' ';
 
+  int exit_code = EXIT_SUCCESS;
   while (fread(&header, sizeof(header), 1, instream)) {
     if (start_timestamp < 0) start_timestamp = header.timestamp_ns;
-    if (header.block_size == 0) continue;
 
-    if (!fread(copy_buf, header.block_size, 1, instream)) {
+    // Don't attempt to read zero bytes when channel is closed.
+    if (header.block_size > 0 &&
+        fread(copy_buf, header.block_size, 1, instream) != 1) {
       fprintf(stderr, "Unexpected end of file reading %d bytes\n",
               header.block_size);
-      return 1;
+      exit_code = EXIT_FAILURE;
+      break;
     }
 
-    if (selected_channels.count(header.channel) == 0)
+    if (selected_channels.count(header.channel) == 0) {
       continue;  // Not interested in printing this channel. Skip.
+    }
 
-    if (header.channel_closed)
-      continue;
-
-    if (print_timestamp != TSPrint::kNone && !last_was_newline)
+    if (print_timestamp != TSPrint::kNone && !last_was_newline) {
       fprintf(out, "\n");  // make sure timestamps start in a new line.
+    }
 
     const int64_t since_start = header.timestamp_ns - start_timestamp;
+    const char *channel_text = PrintChannel(header.channel);
     switch (print_timestamp) {
     case TSPrint::kNone: break;
     case TSPrint::kStartFile:
-      fprintf(out, "%6ld.%06ldms: ",
-              since_start / 1000000, since_start % 1000000);
+      fprintf(out, "%6ld.%06ldms %s: ",
+              since_start / 1000000, since_start % 1000000, channel_text);
       break;
     case TSPrint::kDelta:
-      fprintf(out, "%c%5ld.%06ldms: ", delta_timestamp_prefix,
-              since_start / 1000000, since_start % 1000000);
+      fprintf(out, "%c%5ld.%06ldms %s: ", delta_timestamp_prefix,
+              since_start / 1000000, since_start % 1000000, channel_text);
       delta_timestamp_prefix = '+';
       start_timestamp = header.timestamp_ns;
       break;
@@ -142,17 +214,27 @@ int main(int argc, char *argv[]) {
       localtime_r(&seconds, &prdate);
       char buffer[32];
       strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &prdate);
-      fprintf(out, "[%s.%09ld]: ", buffer, header.timestamp_ns % 1000000000);
+      fprintf(out, "[%s.%09ld] %s: ",
+              buffer, header.timestamp_ns % 1000000000, channel_text);
       break;
     }
     }
 
-    if (print_colored) {
-      write_colored(out, header.channel, copy_buf, header.block_size);
-    } else {
-      fwrite(copy_buf, 1, header.block_size, out);
+    if (header.channel_closed) {
+      if (print_timestamp != TSPrint::kNone) {
+        fprintf(out, "<channel %u closed>\n", header.channel);
+        last_was_newline = true;
+      }
+      continue;
     }
-    last_was_newline = copy_buf[header.block_size - 1] == '\n';
+
+    PrintContent(out, print_opts, header.channel, copy_buf, header.block_size);
+
+    last_was_newline =
+      (copy_buf[header.block_size - 1] == '\n') &&
+      (!print_opts.ascii_escape || print_opts.ascii_escape_break_after_newline);
   }
-  return 0;
+  fclose(instream);
+  fclose(out);
+  return exit_code;
 }
